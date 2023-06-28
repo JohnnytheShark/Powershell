f76f58e3b93dcfb7a884b7539428fe0c305b477f
Jonathan Orellana <Jonathan.Orellana2@nscorp.com>
2023-06-28 10:36:42 -0400

    Update Get-MembersOfGroup.ps1

--- Get-MembersOfGroup.ps1
+++ Get-MembersOfGroup.ps1
@@ -1,4 +1,15 @@
+<# 
+Written By: Jonathan Orellana
+Last Updated: 6/28/2023
+Purpose: The purpose behind the script is to return members of an AD Group, if AD Group is not found it will print out stating it was unable to get members.
+#>
 function Get-MembersOfGroup {
+# Get's the identities for a folder (basically the groups)
+# Creates an array to host the information
+# For loop through the indentities to find the members of each group
+# Transforms the object into a joined list
+# Adds it to array
+# Exports as a CSV
     param($FolderPath)
     $Identities = (get-acl ($FolderPath)).access | Sort-Object IdentityReference
     $data = @()
