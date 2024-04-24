133f105026021348371906cad4b2756763c57141
Jonathan Orellana <Jonathan.Orellana2@nscorp.com>
2024-04-24 08:14:40 -0400

    Automation Learning Script

---
+++ ArchiveFile.ps1
@@ -0,0 +1,26 @@
+﻿# This function is created from reading PracticalAutomation with PowerShell
+# The purpose of the function is to showcase good practices, and to archive files based on being older than a certain point. 
+
+
+Function Set-ArchiveFilePath{
+[CmdletBinding()]
+[OutputType([string])]
+param(
+[Parameter(Mandatory=$true)]
+[string]$ZipPath,
+[Parameter(Mandatory=$true)]
+[string]$ZipPrefix,
+[Parameter(Mandatory=$true)]
+[datetime]$Date
+)
+if(-not (Test_Path -Path $ZipPath){
+    New-Item -Path $ZipPath -ItemType Directory | Out-Null
+    Write-Verbose "Created folder '$ZipPath'"
+}
+$timeString = $Date.ToString('yyyyMMdd')
+$ZipName = "$($ZipPrefix)$($timeString).zip"
+if (Test-Path -Path $ZipPath){
+    throw "The file '$ZipFile' already exists"
+}
+$ZipFile
+}
